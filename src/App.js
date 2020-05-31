8edc2fcefb9b38e9ff3dce9aacb40e043bc1f037
nao <>
2020-05-31 21:52:50 +0900

    learn state and setstate

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 
-//propsは関数に渡せる値。関数側で引数にする。
-const App =() =>{
-  const profiles = [
-    {name:"taro",age:10},
-    {name:"hanako",age:5},
-    {name:"tomo"}//年齢がない場合はデフォルトpropsが使える
-  ]
+//propsは関数に渡せる値。stateはコンポーネントで持っている値。関数側で引数にする。
+const App = () => (<Counter></Counter>)
+
+
+//関数コンポーネント。クラスでの書き方
+class Counter extends Component {
+  constructor(props){//インスタンス初期化時に動く。propsを受け取れる。
+    super(props)
+    //console.log(this.state)
+    this.state = { count:0 }
+  }
+
+  handlePlusButton = () =>{
+    console.log("プラス");
+    //stateをかえるには、必ずsetStateを使うこと。
+    //setstateと同時にrenderが実施される。
+    this.setState({count:this.state.count + 1})
+  }
+
+  handleMinusButton = () =>{
+    console.log("マイナス");
+    //stateをかえるには、必ずsetStateを使うこと。
+    //setstateと同時にrenderが実施される。
+    this.setState({count:this.state.count - 1})
+  }
+
+  render(){
   return(
-  <div>
-    {
-    profiles.map((profile, index)=>{
-      return <User name={profile.name} age={profile.age} key={index}/>
-    })
-    //にゃー!i am taro　私は10さいです
-    //にゃー!i am hanako　私は5さいです
-    //にゃー!i am tomo　私は1さいです
+  <React.Fragment>
+  <div>counter:{this.state.count}</div>
+  <button onClick={this.handlePlusButton}> +1</button>
+  <button onClick={this.handleMinusButton}> -1</button>
+  </React.Fragment>
 
-    }
-  </div>
+ 
   )
-}
 
-//関数コンポーネント。使いまわせる。
-const User = (props) =>{
-return <div>にゃー!i am {props.name}　私は{props.age}さいです</div>
-}
-
-//デフォルトprops、値が不定だった場合に利用できる
-User.defaultProps = {
-  age:1
-}
-
-//"これを追加すれば、userのnameはstringしか受け付けない。
-//バリデーションみたいなもの"
-User.propTypes ={
-  name:PropTypes.string,
-  age:PropTypes.number.isRequired
+  }
 }
 
 export default App;
